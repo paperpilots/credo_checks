625b343e609c8af219aacde04ba25313a31bc213
Luca Hagel <luca@luhagel.com>
2026-04-29 13:13:08 +0200

    Init mix app

---
+++ lib/azav_credo_checks.ex
@@ -0,0 +1,18 @@
+defmodule AzavCredoChecks do
+  @moduledoc """
+  Documentation for `AzavCredoChecks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AzavCredoChecks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/azav_credo_checks_test.exs
@@ -0,0 +1,8 @@
+defmodule AzavCredoChecksTest do
+  use ExUnit.Case
+  doctest AzavCredoChecks
+
+  test "greets the world" do
+    assert AzavCredoChecks.hello() == :world
+  end
+end
