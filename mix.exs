445693445b051efc7a3e24e7c47b74a6919c6cef
Luca Hagel <luca@luhagel.com>
2026-04-29 13:34:12 +0200

    Ops: Add hexdocs

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Azav.CredoChecks.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7"}
+      {:credo, "~> 1.7"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
